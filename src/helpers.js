8d800e0334426940f4eb99ed48299399abffb47c
ffivaz <fabien.fivaz@unine.ch>
2016-09-21 12:06:11 +0200

    initArrays.js: created (contains initial patterns for Game of life). helper functions (helpers.js): renamed (from functions.js) and documented

--- src/helpers.js
+++ src/helpers.js
@@ -1,44 +1,57 @@
-// Warn if overriding existing method
-if(Array.prototype.equals)
+/**
+ * helpers.js
+ * -------------
+ * This file contains helper functions
+ */
+
+
+/**
+ * This function compares arrays
+ * From stackoverflow (thanks to Tomáš Zato)
+ * http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+ */
+
+if (Array.prototype.equals)
     console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-// attach the .equals method to Array's prototype to call it on any array
 Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
     if (!array)
         return false;
 
-    // compare lengths - can save a lot of time
     if (this.length != array.length)
         return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
+    for (var i = 0, l = this.length; i < l; i++) {
         if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
             if (!this[i].equals(array[i]))
                 return false;
         }
         else if (this[i] != array[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
             return false;
         }
     }
     return true;
-}
-// Hide method from for-in loops
+};
+
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
-function arrayClone( arr ) {
+
+/**
+ * This function clones an array
+ * From Stackoverflow (Daniel Lew)
+ * http://stackoverflow.com/questions/597588/how-do-you-clone-an-array-of-objects-in-javascript
+ */
+
+function arrayClone(arr) {
 
     var i, copy;
 
-    if( Array.isArray( arr ) ) {
-        copy = arr.slice( 0 );
-        for( i = 0; i < copy.length; i++ ) {
-            copy[ i ] = arrayClone( copy[ i ] );
+    if (Array.isArray(arr)) {
+        copy = arr.slice(0);
+        for (i = 0; i < copy.length; i++) {
+            copy[i] = arrayClone(copy[i]);
         }
         return copy;
-    } else if( typeof arr === 'object' ) {
+    } else if (typeof arr === 'object') {
         throw 'Cannot clone array containing an object!';
     } else {
         return arr;
---
+++ src/initArrays.js
@@ -0,0 +1,101 @@
+/**
+ * initArrays.js
+ * -------------
+ * This file contains a set of initial situations.
+ * TODO: create pattern around a center of canvas (not hard coded as it is now)
+ */
+
+var initArray = function (someCells, type) {
+
+    switch (type) {
+        case 'blinker':
+            someCells[20][35] = 1;
+            someCells[20][36] = 1;
+            someCells[20][37] = 1;
+            break;
+        case 'bloc':
+            someCells[20][35] = 1;
+            someCells[21][35] = 1;
+            someCells[20][36] = 1;
+            someCells[21][36] = 1;
+            break;
+        case 'toad':
+            someCells[20][35] = 1;
+            someCells[20][36] = 1;
+            someCells[20][37] = 1;
+            someCells[21][34] = 1;
+            someCells[21][35] = 1;
+            someCells[21][36] = 1;
+            break;
+        case 'eight-in-line':
+            someCells[20][35] = 1;
+            someCells[20][36] = 1;
+            someCells[20][37] = 1;
+            someCells[20][38] = 1;
+            someCells[20][39] = 1;
+            someCells[20][40] = 1;
+            someCells[20][41] = 1;
+            someCells[20][42] = 1;
+            break;
+        case 'acorn':
+            someCells[19][34] = 1;
+            someCells[20][36] = 1;
+            someCells[21][33] = 1;
+            someCells[21][34] = 1;
+            someCells[21][37] = 1;
+            someCells[21][38] = 1;
+            someCells[21][39] = 1;
+            break;
+        case 'random':
+            for (var j = 1; j < someCells.length - 1; j++) {
+                for (var i = 1; i < someCells[j].length - 1; i++) {
+                    someCells[j][i] = Math.round(Math.random());
+                }
+            }
+            break;
+        case 'glider':
+            someCells[19][34] = 1;
+            someCells[20][35] = 1;
+            someCells[20][36] = 1;
+            someCells[21][34] = 1;
+            someCells[21][35] = 1;
+            break;
+        case 'gosper-glider-gun':
+            someCells[17][35] = 1;
+            someCells[18][33] = 1;
+            someCells[18][35] = 1;
+            someCells[19][23] = 1;
+            someCells[19][24] = 1;
+            someCells[19][31] = 1;
+            someCells[19][32] = 1;
+            someCells[19][45] = 1;
+            someCells[19][46] = 1;
+            someCells[20][22] = 1;
+            someCells[20][26] = 1;
+            someCells[20][31] = 1;
+            someCells[20][32] = 1;
+            someCells[20][45] = 1;
+            someCells[20][46] = 1;
+            someCells[21][11] = 1;
+            someCells[21][12] = 1;
+            someCells[21][21] = 1;
+            someCells[21][27] = 1;
+            someCells[21][31] = 1;
+            someCells[21][32] = 1;
+            someCells[22][11] = 1;
+            someCells[22][12] = 1;
+            someCells[22][21] = 1;
+            someCells[22][25] = 1;
+            someCells[22][27] = 1;
+            someCells[22][28] = 1;
+            someCells[22][33] = 1;
+            someCells[22][35] = 1;
+            someCells[23][21] = 1;
+            someCells[23][27] = 1;
+            someCells[23][35] = 1;
+            someCells[24][22] = 1;
+            someCells[24][26] = 1;
+            someCells[25][23] = 1;
+            someCells[25][24] = 1;
+    }
+};
